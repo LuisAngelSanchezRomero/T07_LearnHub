35c76e11e5e4fad06e5f0ef26c60691cbe91924d
LuisSánchez <luis.sanchez.romero@vallegrande.edu.pe>
2025-09-29 10:16:45 -0500

    Update 7_dev2_constraints.sql

--- Development/7_dev2_constraints.sql
+++ Development/7_dev2_constraints.sql
@@ -1,19 +1,16 @@
--- Constraints de Karla2
+-- Relaciones desde Karla2 hacia Lucas1
+ALTER TABLE enrollment
+ADD CONSTRAINT fk_enrollment_student FOREIGN KEY (student_code)
+REFERENCES Lucas1.student(student_code);
 
--- Relación module (Karla2) → course (Lucas1)
+ALTER TABLE enrollment
+ADD CONSTRAINT fk_enrollment_course FOREIGN KEY (course_code)
+REFERENCES Lucas1.course(course_code);
 
-ALTER TABLE Karla2.module
+ALTER TABLE module
 ADD CONSTRAINT fk_module_course FOREIGN KEY (course_code)
 REFERENCES Lucas1.course(course_code);
 
--- Relación student_progress (Luis3) → enrollment (Karla2)
-
-ALTER TABLE Luis3.student_progress
-ADD CONSTRAINT fk_progress_enrollment FOREIGN KEY (enrollment_id)
-REFERENCES Karla2.enrollment(enrollment_id);
-
--- Relación student_progress (Luis3) → module (Karla2)
-
-ALTER TABLE Luis3.student_progress
-ADD CONSTRAINT fk_progress_module FOREIGN KEY (module_id)
-REFERENCES Karla2.module(module_id);
+-- Dar permisos a Luis3
+GRANT SELECT, REFERENCES ON enrollment TO Luis3;
+GRANT SELECT, REFERENCES ON module TO Luis3;
