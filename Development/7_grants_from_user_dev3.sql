7aab451414007f0e271adf190df3a650728c86cd
LuisSánchez <luis.sanchez.romero@vallegrande.edu.pe>
2025-10-02 14:39:53 +0000

    Update 13_dev3_load_data.sql

---
+++ Development/7_grants_from_user_dev3.sql
@@ -0,0 +1,5 @@
+-- Permisos que Luis3 doy a Lucas1, Karla2
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON student_progress TO Lucas1;
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON student_progress TO Karla2;
